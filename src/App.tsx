c01cb45a0cb812dd26f90f1943a559c1ac9d0224
Sayali Kanase <58733849+sayali1910@users.noreply.github.com>
2023-04-28 07:46:59 +0000

    Add routing for book card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import BookList from "./components/BookList";
+import BookList from "./components/bookList";
+import BookCard from "./components/bookCard";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BookList />
+      <Routes>
+        <Route path="/" element={<BookList />} />
+        <Route path="/book" element={<BookCard />} />
+      </Routes>
     </div>
   );
 }
